e496ad263de0c9a29afd8107ed9fe7421a54bc0c
afomina <Anastasia.Fomina@ihs.com>
2018-01-31 17:49:24 +0300

    change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,13 +22,33 @@ class Item{
 export class AppComponent { 
     items: Item[] = 
     [
+        { task: "wake up", done: false, date: "2018-01-01" },
         { task: "work", done: false, date: "2018-01-01" },
-        { task: "sleep", done: false, date: "2018-01-01" }
+        { task: "eat", done: false, date: "2018-01-01" },
+        { task: "sleep", done: false, date: "2018-01-01" },
+        { task: "repeat", done: false, date: "2018-01-01" }
     ];
     addItem(text: string, date: string): void {
         if(!text || !date)
             return;
         this.items.push(new Item(text,date));
+        console.log(this.items);
+    };
+    checkTask(item:{task: string;done: boolean;date :string;}){
+        item.done = !item.done;
+        for (let i = 0; i < this.items.length; i++) {
+            if(this.items[i] === item){
+                this.items.splice(i,1);
+                this.items.push(item);
+            } 
+        } 
+    };
+    removeTask(item:{}){
+        for (let i = 0; i < this.items.length; i++) {
+            if(this.items[i] === item){
+                this.items.splice(i,1);
+            } 
+        } 
     }
 }
 
